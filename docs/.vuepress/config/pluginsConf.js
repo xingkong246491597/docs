26f08deeee3f82ad54cc914b7dc27e53845f23fa
“xingkong246491597” <2464915974@qq.com>
2021-02-03 16:51:19 +0800

    修改搜索支持全局查找

--- docs/.vuepress/config/pluginsConf.js
+++ docs/.vuepress/config/pluginsConf.js
@@ -4,7 +4,7 @@ module.exports = {
     '@vuepress/last-updated':
     {
       transformer: (timestamp) => {
-        // 不要忘了安装 moment
+        // 不要忘了安装 moment 显示更新时间
         const moment = require('moment')
         return moment(timestamp).format('LLLL')
       }
@@ -19,5 +19,9 @@ module.exports = {
       //启动自动生成侧边栏
       "vuepress-plugin-auto-sidebar": {
         nav: true
+      },
+      //搜索插件
+      'flexsearch-pro':{
+        
       }
 }
